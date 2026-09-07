95c4eded7e305fcf30932e650f6ae588c3184e71
Aqib1012 <154654435+Aqib1012@users.noreply.github.com>
2026-09-07 16:54:39 +0500

    Remove custom headers from next.config.js
    Removed custom headers configuration from next.config.js.

--- next.config.js
+++ next.config.js
@@ -12,17 +12,6 @@ const nextConfig = {
     }
     return config;
   },
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
