6dba7e41d098f21c12293559ce139c52bb3a8e79
Aqib1012 <154654435+Aqib1012@users.noreply.github.com>
2026-09-09 09:04:23 +0500

    Integrate Google Tag Manager into layout
    Added Google Tag Manager scripts for tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,8 +135,29 @@ export default function RootLayout({
             }),
           }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W25FK72J');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-body bg-cream text-ink antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W25FK72J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
